d43a6ec54021e3910736d59b1e4b820abad104a2
a <skyandrd@gmail.com>
2019-09-24 21:46:06 +0500

    Updates

---
+++ precache-manifest.c2686974b7447c60129bb5c0581bfc55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/audio-status-vk/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "65fa77ec9d7ac31ba06a",
+    "url": "/audio-status-vk/static/js/runtime~main.add8af69.js"
+  },
+  {
+    "revision": "6fedf5e81b1b1ccd47ef",
+    "url": "/audio-status-vk/static/js/main.2d77d856.chunk.js"
+  },
+  {
+    "revision": "b3354895ac4660ab0fed",
+    "url": "/audio-status-vk/static/js/2.aa6dc732.chunk.js"
+  },
+  {
+    "revision": "6fedf5e81b1b1ccd47ef",
+    "url": "/audio-status-vk/static/css/main.c1e3a0b1.chunk.css"
+  },
+  {
+    "revision": "b3354895ac4660ab0fed",
+    "url": "/audio-status-vk/static/css/2.a069024b.chunk.css"
+  },
+  {
+    "revision": "8b6b9d19acdaed0cd6c446c0e7da7104",
+    "url": "/audio-status-vk/index.html"
+  }
+];
